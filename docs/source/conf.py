54753cad91558799c691211d180284f73b916500
Chris <chris@clyates.plus.com>
2026-02-23 15:53:04 +0000

    Update documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Yii Chartist'
+project = 'Yii-Chartist'
 copyright = '2026, BeastBytes'
 author = 'BeastBytes'
 
